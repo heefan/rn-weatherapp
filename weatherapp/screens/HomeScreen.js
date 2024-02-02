ec98b6b36fd250b5bc8925998a03dd91133a1601
Li_Tian <Li_Tian@singaporeair.com.sg>
2024-02-02 15:01:30 +0800

    close #2, cleanup the template and put a empty home screen [2]

---
+++ weatherapp/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Home Screen</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
