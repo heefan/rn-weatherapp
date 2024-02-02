a2902057c373dd05880648b63ddea34f716ec155
Li_Tian <Li_Tian@singaporeair.com.sg>
2024-02-02 20:03:46 +0800

    #4, search bar, add outline view [4]

--- weatherapp/screens/HomeScreen.js
+++ weatherapp/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import {View, StatusBar, Image } from 'react-native';
+import {View, StatusBar, Image, SafeAreaView, TextInput} from 'react-native';
+import { theme } from "../theme/index";
 
 export default function HomeScreen() {
     return (
@@ -10,6 +11,16 @@ export default function HomeScreen() {
                 blurRadius={70}
                 source={require("../assets/images/bg.png")}
                 style={{width: '100%', height: '100%', position: 'absolute'}} />
+
+            <SafeAreaView className={"flex flex-1"} style={{backgroundColor: "red"}}>
+                {/* search area */}
+                <View style={{backgroundColor: "blue"}}>
+                    <View style={{backgroundColor: "green"}}>
+                        <TextInput placeholder={'Search City'} placeholderTextColor={'lightgrey'} />
+                    </View>
+                </View>
+            </SafeAreaView>
         </View>
+
     );
 }
