1c4f5c3e850f678053110bb593d51618c8b9b941
Li_Tian <Li_Tian@singaporeair.com.sg>
2024-02-02 17:39:16 +0800

    close #3, add background image for HomeScreen [3]

--- weatherapp/screens/HomeScreen.js
+++ weatherapp/screens/HomeScreen.js
@@ -1,12 +1,15 @@
+
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import {View, StatusBar, Image } from 'react-native';
 
 export default function HomeScreen() {
     return (
-        <SafeAreaView>
-            <View>
-                <Text>Home Screen</Text>
-            </View>
-        </SafeAreaView>
+        <View style={{flex: 1, position: 'relative'}}>
+            <StatusBar barStyle={"light-content"} />
+            <Image
+                blurRadius={70}
+                source={require("../assets/images/bg.png")}
+                style={{width: '100%', height: '100%', position: 'absolute'}} />
+        </View>
     );
 }
