8beb7ebdff8e2f3f2dcc6341955378c3bccf970b
li_tian <heefan@msn.com>
2024-02-03 15:38:41 +0800

    close #5, fix NativeWind config issue [5]

--- weatherapp/screens/HomeScreen.js
+++ weatherapp/screens/HomeScreen.js
@@ -1,22 +1,31 @@
 
 import React from 'react';
-import {View, StatusBar, Image, SafeAreaView, TextInput} from 'react-native';
+import {View, StatusBar, Image, SafeAreaView, TextInput, Touchable, TouchableOpacity} from 'react-native';
 import { theme } from "../theme/index";
 
 export default function HomeScreen() {
     return (
         <View style={{flex: 1, position: 'relative'}}>
-            <StatusBar barStyle={"light-content"} />
+            <StatusBar style="light" />
             <Image
                 blurRadius={70}
                 source={require("../assets/images/bg.png")}
-                style={{width: '100%', height: '100%', position: 'absolute'}} />
+                className="absolute h-full w-full" />
 
-            <SafeAreaView className={"flex flex-1"} style={{backgroundColor: "red"}}>
-                {/* search area */}
-                <View style={{backgroundColor: "blue"}}>
-                    <View style={{backgroundColor: "green"}}>
-                        <TextInput placeholder={'Search City'} placeholderTextColor={'lightgrey'} />
+            <SafeAreaView className={"flex flex-1 bg-blue-950"}>
+                {/* Make a search bar */}
+                <View className={"mx-4 relative z-50 bg-red-700"}>
+                    <View
+                        className={"flex-row justify-end items-center rounded-full"}
+                        style={{backgroundColor: theme.bgWhite(1)}}>
+
+                        <TextInput
+                            className={"text-white text-base pb-1 flex-1 h-10 pl-6"}
+                            placeholder="Search City"
+                            placeholderTextColor={"lightgray"}
+                        />
+
+                        <TouchableOpacity></TouchableOpacity>
                     </View>
                 </View>
             </SafeAreaView>
