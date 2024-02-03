3d26936be4ad4a0304d88144ebd925629489e3df
li_tian <heefan@msn.com>
2024-02-03 17:26:11 +0800

    close #4, add search bar view [4]

--- weatherapp/screens/HomeScreen.js
+++ weatherapp/screens/HomeScreen.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {View, StatusBar, Image, SafeAreaView, TextInput, Touchable, TouchableOpacity} from 'react-native';
 import { theme } from "../theme/index";
+import { MagnifyingGlassIcon } from "react-native-heroicons/mini";
 
 export default function HomeScreen() {
     return (
@@ -12,12 +13,12 @@ export default function HomeScreen() {
                 source={require("../assets/images/bg.png")}
                 className="absolute h-full w-full" />
 
-            <SafeAreaView className={"flex flex-1 bg-blue-950"}>
+            <SafeAreaView className={"flex flex-1"}>
                 {/* Make a search bar */}
-                <View className={"mx-4 relative z-50 bg-red-700"}>
+                <View className={"mx-4 relative z-50"}>
                     <View
                         className={"flex-row justify-end items-center rounded-full"}
-                        style={{backgroundColor: theme.bgWhite(1)}}>
+                        style={{backgroundColor: theme.bgWhite(0.2)}}>
 
                         <TextInput
                             className={"text-white text-base pb-1 flex-1 h-10 pl-6"}
@@ -25,7 +26,12 @@ export default function HomeScreen() {
                             placeholderTextColor={"lightgray"}
                         />
 
-                        <TouchableOpacity></TouchableOpacity>
+                        <TouchableOpacity
+                            style={{backgroundColor: theme.bgWhite(0.3)}}
+                            className={"rounded-full p-3 m-1"}>
+
+                            <MagnifyingGlassIcon size="25" color="white" />
+                        </TouchableOpacity>
                     </View>
                 </View>
             </SafeAreaView>
