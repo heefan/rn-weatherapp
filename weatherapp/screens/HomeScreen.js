310a61f76e12bd0267ccc515f4b1120994438fe0
li_tian <heefan@msn.com>
2024-02-03 17:42:58 +0800

    close #5, add search function [6]

--- weatherapp/screens/HomeScreen.js
+++ weatherapp/screens/HomeScreen.js
@@ -3,8 +3,11 @@ import React from 'react';
 import {View, StatusBar, Image, SafeAreaView, TextInput, Touchable, TouchableOpacity} from 'react-native';
 import { theme } from "../theme/index";
 import { MagnifyingGlassIcon } from "react-native-heroicons/mini";
+import { useState } from 'react';
 
 export default function HomeScreen() {
+    const [showSearch, toggleSearch] = useState(false)
+
     return (
         <View style={{flex: 1, position: 'relative'}}>
             <StatusBar style="light" />
@@ -18,15 +21,19 @@ export default function HomeScreen() {
                 <View className={"mx-4 relative z-50"}>
                     <View
                         className={"flex-row justify-end items-center rounded-full"}
-                        style={{backgroundColor: theme.bgWhite(0.2)}}>
-
-                        <TextInput
-                            className={"text-white text-base pb-1 flex-1 h-10 pl-6"}
-                            placeholder="Search City"
-                            placeholderTextColor={"lightgray"}
-                        />
+                        style={{backgroundColor: showSearch ? theme.bgWhite(0.2) : "transparent"}}>
+                        {
+                            showSearch? (
+                                <TextInput
+                                    className={"text-white text-base pb-1 flex-1 h-10 pl-6"}
+                                    placeholder="Search City"
+                                    placeholderTextColor={"lightgray"}
+                                />
+                            ): null
+                        }
 
                         <TouchableOpacity
+                            onPress={() => toggleSearch(!showSearch)}
                             style={{backgroundColor: theme.bgWhite(0.3)}}
                             className={"rounded-full p-3 m-1"}>
 
