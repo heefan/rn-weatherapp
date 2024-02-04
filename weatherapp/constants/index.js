5dc73b998efcb9c14e0ff2959a03cb37f8c91dc7
li_tian <heefan@msn.com>
2024-02-04 11:07:48 +0800

    close #10, add weather api endpoint [10]

---
+++ weatherapp/constants/index.js
@@ -0,0 +1,19 @@
+import Config from 'react-native-config';
+export const apiKey = Config.API_KEY;
+
+export const WeatherImages = {
+    'Partly Cloudy': require('../assets/images/partlycloudy.png'),
+    'Moderate rain': require('../assets/images/moderaterain.png'),
+    'Patchy rain possible': require('../assets/images/moderaterain.png'),
+    'Sunny': require('../assets/images/sunny.png'),
+    'Clear': require('../assets/images/sunny.png'),
+    'Overcast': require('../assets/images/cloud.png'),
+    'Cloudy': require('../assets/images/cloud.png'),
+    'Light rain at times': require('../assets/images/moderaterain.png'),
+    'Heavy rain': require('../assets/images/heavyrain.png'),
+    'Heavy rain at times': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy freezing rain': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain shower': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/images/heavyrain.png'),
+    'other': require('../assets/images/sunny.png'),
+}
